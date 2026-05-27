caa37a2002cddf3c85a715787b7a50ced03b1f5c
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-05-27 17:02:29 -0300

    API FINALIZADA

--- cadastrar-cliente/index.js
+++ cadastrar-cliente/index.js
@@ -8,7 +8,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post('/cadastrar', async(req, res) => {
+    const Nome = req.body.nome;
+    const DataNascimento = req.body.dataNascimento;
+    const Telefone = req.body.telefone;
+    const Sexo = req.body.sexo;
 
+    const clienteValido = validaUsuario(Nome, DataNascimento, Telefone, Sexo);
+
+    if(clienteValido.status) {
+        await cadastrarClientes(Nome, DataNascimento, Telefone, Sexo);
+        res.status(201).send({mensagem: 'Cliente Cadastrado com sucesso'});
+    }
+    else 
+    {
+        res.status(400).send({mensagem: clienteValido.mensagem})
+
+    }
+
+})
 
 app.listen(3001, async() => {
     

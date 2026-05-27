f5949f2c107a9643e50df807d5abbc569929d80c
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-05-27 16:24:39 -0300

    testando a conexao com o banco de dados

---
+++ cadastrar-cliente/servico/conexao.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise';
+import env from 'dotenv';
+
+env.config();
+
+const pool = mysql.createPool({
+    host: process.env.CS_HOST,
+    user: process.env.CS_USER,
+    password: process.env.CS_PASSWORD,
+    database: process.env.CS_DATABASE
+})
+
+export default pool;
